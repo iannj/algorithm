3ecce0a9d17aef3b133f9f9b4de49587b64b9e09
iannj <liujie.nj@gmail.com>
2020-06-26 15:48:13 +0800

    dtw source code

---
+++ Trajectory Similarities/DTW/dtw.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+This is the source code of pierre-rouanet's dtw 1.4 on GitHub 1 year ago.
+I rewrite some codes and add many comments to help myself to understand the codes.
+As sklearn.metrics changes it's parameters, I rewrite those parts too.
+
+The origin code can be found at https://github.com/pierre-rouanet/dtw/blob/master/dtw/dtw.py.
+
+"""
